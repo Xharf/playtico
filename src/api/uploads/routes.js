bcacef0507b2ef731efd0a23191e45d8b37c9c9b
xharf <123190123@student.upnyk.ac.id>
2022-05-09 09:50:05 +0700

    test

--- src/api/uploads/routes.js
+++ src/api/uploads/routes.js
@@ -8,7 +8,7 @@ const routes = (handler) => [
         allow: 'multipart/form-data',
         multipart: true,
         output: 'stream',
-        maxBytes: 500000,
+        maxBytes: 1000000,
       },
     },
   },
